370c6befe1836b9aa85c803618783b92abe99672
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-08-31 01:40:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@
 
 # install homebrew (automatically installs xcode command line tools)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# for apple silicon: Set PATH, MANPATH, etc., for Homebrew:
-echo "eval \"$(/opt/homebrew/bin/brew shellenv)\"" >> ~/.zprofile
+# for apple silicon:
+echo "# Set PATH, MANPATH, etc., for Homebrew." >> ~/.zprofile
+echo "eval \"\$(/opt/homebrew/bin/brew shellenv)\"" >> ~/.zprofile
+source ~/.zprofile
 
 # install brewfile
 brew bundle --file=./homebrew/Brewfile
+
+# create symlinks with stow
+cd ./symlinks && stow -t ~/ . && cd ..
