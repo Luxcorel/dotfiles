86f94300e4841ee7a1664cf526f970af96423b41
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2025-12-10 22:26:11 +0100

    feat: add pnpm to path

--- symlinks/.zshrc.d/03-env-vars.zsh
+++ symlinks/.zshrc.d/03-env-vars.zsh
@@ -2,3 +2,13 @@
 export LANG="en_US.UTF-8"
 is_installed nvim && export EDITOR=nvim
 is_installed nvim && export VISUAL=nvim
+
+is_macos && {
+  export PNPM_HOME="$HOME/Library/pnpm"
+  if [ -d "$PNPM_HOME" ]; then
+    case ":$PATH:" in
+      *":$PNPM_HOME:"*) ;;
+      *) export PATH="$PNPM_HOME:$PATH" ;;
+    esac
+  fi
+}
