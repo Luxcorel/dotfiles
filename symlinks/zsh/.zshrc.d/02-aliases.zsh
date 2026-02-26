83c47784f3071000fe75b8b3b3a22ffaddc2baab
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2026-02-27 00:22:03 +0100

    feat(zsh): add copy/paste aliases for wl-clipboard

--- symlinks/zsh/.zshrc.d/02-aliases.zsh
+++ symlinks/zsh/.zshrc.d/02-aliases.zsh
@@ -5,6 +5,15 @@ is_macos && {
   alias v="pbpaste"                           # macos clipboard paste
 }
 
+is_linux && {
+  is_installed wl-copy && is_installed wl-paste && {
+    alias C="tr -d '\n' | wl-copy"
+    alias V="wl-paste"
+    alias C0="tr -d '\n' | wl-copy --primary"
+    alias V0="wl-paste --primary"
+  }
+}
+
 is_installed eza && alias ll="eza -al --icons --git"
 ! is_installed eza && alias ll="ls -ahlF --color=auto"
 is_installed bat && alias cat="bat --theme Coldark-Dark"
