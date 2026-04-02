d7c7a49a5b55201625d69ad67fec8f7b622dcdef
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2026-04-02 23:54:19 +0200

    feat(zsh): add procrastination option for todo function

--- symlinks/zsh/.zshrc.d/todo.zsh
+++ symlinks/zsh/.zshrc.d/todo.zsh
@@ -3,11 +3,21 @@ is_installed nvim || return
 todo() {
     local todo_dir="$HOME/.todo"
     if [[ ! -d "$todo_dir" ]]; then
-        mkdir -p "$todo_dir"
+        mkdir -p "$todo_dir" || return 1
     fi
 
     local filename
-    filename="todo-$(date +'%Y-%m-%d').txt"
+    case "$1" in
+    +1)
+        filename="todo-$(date -v +1d +'%Y-%m-%d').txt" || return 1
+        ;;
+    -1)
+        filename="todo-$(date -v -1d +'%Y-%m-%d').txt" || return 1
+        ;;
+    *)
+        filename="todo-$(date +'%Y-%m-%d').txt" || return 1
+        ;;
+    esac
 
     local filepath="$todo_dir/$filename"
 
